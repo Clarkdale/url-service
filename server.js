e01020c69bc688e1a5f8dfc5791c5e96fdf3f9b0
Clarkdale <clarkdale@email.arizona.edu>
2020-05-20 14:49:40 -0700

    Update server url add

--- server.js
+++ server.js
@@ -22,12 +22,16 @@ app.get('/addUrl/:url', (request, response) => {
     let url = request.params.url;
     db.getUrlId(url)
         .then(x => {
-            console.log(x);
+            let res = JSON.parse(x);
+            if (res.length > 0) {
+                response.json(res[0].id);
+            } else {
+
+            }
+            db.addUrl(url);
+            db.getUrlId(url).then(y => response.json(y));
         })
-        //.catch(e => {console.trace(); response.status(500).send(e)});
-    //db.addUrl(url)
-      //  .then(x => response.json(x))
-        //.catch(e => {console.trace(); response.status(500).send(e)});
+        .catch(e => {console.trace(); response.status(500).send(e)});
 });
 
 app.get('/getUrl/:id', (request, response) => {
