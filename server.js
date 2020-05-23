73f1ca6aca9fd46a85d174ee8f6ca63ee37ebb51
Clarkdale <clarkdale@email.arizona.edu>
2020-05-23 16:16:12 -0700

    Change array to string

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 4003;
 
 app.use(cors());
 
-var map = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'E', 'F', 'G', 'H',
-                       'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 
-                       'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                       'y', 'z'];
+var map = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 app.get('/r/:urlId', (request, response) => {
     let curr;
