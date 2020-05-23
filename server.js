b449dee759c9ef4f1934677eeba636f63a3d4913
Clarkdale <clarkdale@email.arizona.edu>
2020-05-23 16:12:48 -0700

    try loop again

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ app.get('/r/:urlId', (request, response) => {
     let curr;
     let init = request.params.urlId;
     let result = 0;
-    for (let i = 0, j = init.length; i < init.length, j >= 0; i++, j--) {
+
+    for (let i = 0, j = init.length - 1; i < init.length, j >= 0; i++, j--) {
         curr = map.indexOf(init[i]);
-        result += (curr * Math.pow(61, i));
+        result += (curr * Math.pow(61, j));
     }
     console.log(result);
     db.getIdUrl(request.params.urlId)
