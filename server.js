fe1c2ca2ad66dfcfdc609fdd1fdc08a93f47acdd
Clarkdale <clarkdale@email.arizona.edu>
2020-05-23 16:13:37 -0700

    fix syntax

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/r/:urlId', (request, response) => {
     let init = request.params.urlId;
     let result = 0;
 
-    for (let i = 0, j = init.length - 1; i < init.length, j >= 0; i++, j--) {
+    for (let i = 0, j = init.length - 1; i < init.length && j >= 0; i++, j--) {
         curr = map.indexOf(init[i]);
         result += (curr * Math.pow(61, j));
     }
