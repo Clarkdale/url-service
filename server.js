864dd8da8b70e409e525c9b4d8aefca4810381a9
Clarkdale <clarkdale@email.arizona.edu>
2020-05-24 15:38:15 -0700

    Redirect functionality moved to different endpoint

--- server.js
+++ server.js
@@ -10,24 +10,6 @@ const port = process.env.PORT || 4003;
 
 app.use(cors());
 
-var map = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-app.get('/r/:urlId', (request, response) => {
-    let curr;
-    let init = request.params.urlId;
-    let result = 0;
-
-    for (let i = 0, j = init.length - 1; i < init.length && j >= 0; i++, j--) {
-        curr = map.indexOf(init[i]);
-        result += (curr * Math.pow(61, j));
-    }
-    db.getIdUrl(result)
-        .then(x => {
-            response.redirect(x);
-        })
-        .catch(e => {console.trace(); response.status(500).send('Could not find the designated link')});
-});
-
 app.get('/allUrls', (request, response) => {
     db.getAllUrls()
     .then(x => response.json(x))
