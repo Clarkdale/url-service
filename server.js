c8d8b753c5f9bb7d1c8f34430151aca31c02dd49
Clarkdale <clarkdale@email.arizona.edu>
2020-05-26 09:11:15 -0700

    Wrong get request changed

--- server.js
+++ server.js
@@ -8,26 +8,11 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const app = express();
 const port = process.env.PORT || 4003;
 
-const map = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
 app.use(cors());
 
 app.get('/allUrls', (request, response) => {
     db.getAllUrls()
-    .then(x => {
-        let vals = [];
-        let out = "";
-        while (x > 0) {
-            vals.push(x % 61);
-            x = Math.floor(x / 61);
-        }
-        vals.reverse();
-                            
-        for (let i = 0; i < vals.length; i++) {
-            out += map[vals[i]];
-        }
-        response.json(out)
-    })
+    .then(x => response.json(x))
     .catch(e => {console.trace(); response.status(500).send('The databse data could not be retrieved.')});
 });
 
@@ -36,7 +21,20 @@ app.get('/addUrl/:url(*)', (request, response) => {
     db.addUrl(url)
         .then(x => {
             db.getUrlId(url)
-                .then(y => response.json(y));
+                .then(y => {
+                    let vals = [];
+                    let out = "";
+                    while (y > 0) {
+                        vals.push(y % 61);
+                        y = Math.floor(y / 61);
+                    }
+                    vals.reverse();
+                                    
+                    for (let i = 0; i < vals.length; i++) {
+                        out += map[vals[i]];
+                    }
+                    response.json(out)
+                });
         })
         .catch(e => {console.trace(); response.status(500).send('The url could not be added')});
 });
